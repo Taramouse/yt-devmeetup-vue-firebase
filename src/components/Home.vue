07c3a5d3cb88e13ec060d9187e276554da512508
taramouse <support@optimized4u.org>
2018-01-01 21:41:17 +0000

    Rewind to episode 6.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+
     <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
         <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
@@ -8,6 +9,7 @@
         <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
       </v-flex>
     </v-layout>
+
     <v-layout>
       <v-flex xs12 class="text-xs-center">
         <v-progress-circular
@@ -15,9 +17,11 @@
           class="primary--text"
           :width="7"
           :size="70"
-          v-if="loading"></v-progress-circular>
+          v-if="loading">
+        </v-progress-circular>
       </v-flex>
     </v-layout>
+
     <v-layout row wrap class="mt-2" v-if="!loading">
       <v-flex xs12>
         <v-carousel style="cursor: pointer;">
@@ -33,6 +37,7 @@
         </v-carousel>
       </v-flex>
     </v-layout>
+
     <v-layout row wrap  class="mt-2">
       <v-flex xs12 class="text-xs-center">
        <p>Join our awesome meetups!</p>
@@ -43,19 +48,28 @@
 
 <script>
   export default {
-    computed: {
-      meetups () {
-        return this.$store.getters.featuredMeetups
-      },
-      loading () {
-        return this.$store.getters.loading
-      }
-    },
-    methods: {
-      onLoadMeetup (id) {
-        this.$router.push('/meetups/' + id)
+    data () {
+      return {
+        meetups: [
+          { imageUrl: 'http://images.huffingtonpost.com/2013-10-24-72263318.jpg', id: '1', title: 'Meeting in Painswick' },
+          { imageUrl: 'http://www.coolplaces.co.uk/system/images/6091/stroud-farmers-market-shop-food-drink-large.jpg', id: '2', title: 'Meeting in Stroud' },
+          { imageUrl: 'http://www.thecityofgloucester.co.uk/imageresizer/?image=%2Fdbimgs%2Fimg_home_mainimggallery_1.jpg&action=ET-MediaBackgroundHome', id: '3', title: 'Meeting in Gloucester' }
+        ]
       }
     }
+    // computed: {
+    //   meetups () {
+    //     return this.$store.getters.featuredMeetups
+    //   },
+    //   loading () {
+    //     return this.$store.getters.loading
+    //   }
+    // },
+    // methods: {
+    //   onLoadMeetup (id) {
+    //     this.$router.push('/meetups/' + id)
+    //   }
+    // }
   }
 </script>
 
