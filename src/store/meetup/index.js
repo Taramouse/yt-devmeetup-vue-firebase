ca1e48d0e42463e1b99297646f78f25b93482f83
taramouse <support@optimized4u.org>
2018-01-11 18:34:23 +0000

    Split the Vuex store into modules.

---
+++ src/store/meetup/index.js
@@ -0,0 +1,160 @@
+import * as firebase from 'firebase'
+
+export default {
+  state: {
+    loadedMeetups: [
+      {
+        imageUrl: 'http://images.huffingtonpost.com/2013-10-24-72263318.jpg',
+        id: '1',
+        title: 'Dummy Vue.js meetup in Painswick',
+        date: '2018-01-20',
+        location: 'Painswick',
+        description: 'Vue, Vuetify and Firebase javascript workshop'
+      },
+      {
+        imageUrl: 'http://www.coolplaces.co.uk/system/images/6091/stroud-farmers-market-shop-food-drink-large.jpg',
+        id: '2',
+        title: 'Dummy Vue.js meetup in Stroud',
+        date: '2018-01-22',
+        location: 'Stroud',
+        description: 'Vue, Vuetify and Firebase javascript workshop'
+      },
+      {
+        imageUrl: 'http://www.thecityofgloucester.co.uk/imageresizer/?image=%2Fdbimgs%2Fimg_home_mainimggallery_1.jpg&action=ET-MediaBackgroundHome',
+        id: '3',
+        title: 'Dummy Vue.js meetup in Gloucester',
+        date: '2018-01-24',
+        location: 'Gloucester',
+        description: 'Vue, Vuetify and Firebase javascript workshop'
+      }
+    ]
+  },
+  mutations: {
+    setLoadedMeetups (state, payload) {
+      state.loadedMeetups = payload
+    },
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    },
+    updateMeetup (state, payload) {
+      const meetup = state.loadedMeetups.find(meetup => {
+        return meetup.id === payload.id
+      })
+      if (payload.title) {
+        meetup.title = payload.title
+      }
+      if (payload.description) {
+        meetup.description = payload.description
+      }
+      if (payload.date) {
+        meetup.date = payload.date
+      }
+    }
+  },
+  actions: {
+    loadMeetups ({commit}) {
+      commit('setLoading', true)
+      // replace once with on for realtime updates from firebase.
+      firebase.database().ref('meetups').once('value')
+        .then((data) => {
+          const meetups = []
+          const obj = data.val()
+          for (let key in obj) {
+            meetups.push({
+              id: key,
+              title: obj[key].title,
+              description: obj[key].description,
+              location: obj[key].location,
+              imageUrl: obj[key].imageUrl,
+              date: obj[key].date,
+              creatorId: obj[key].creatorId
+            })
+          }
+          commit('setLoadedMeetups', meetups)
+          commit('setLoading', false)
+        })
+        .catch(
+          (error) => {
+            console.log(error)
+            commit('setLoading', true)
+          }
+        )
+    },
+    createMeetup ({ commit, getters }, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        description: payload.description,
+        date: payload.date.toISOString(),
+        creatorId: getters.user.id
+      }
+      let imageUrl
+      let key
+      firebase.database().ref('meetups').push(meetup)
+        .then((data) => {
+          key = data.key
+          return key
+        })
+        .then(key => {
+          const filename = payload.image.name
+          const ext = filename.slice(filename.lastIndexOf('.'))
+          return firebase.storage().ref('meetups' + key + '.' + ext).put(payload.image)
+        })
+        .then(fileData => {
+          imageUrl = fileData.metadata.downloadURLs[0]
+          return firebase.database().ref('meetups').child(key).update({ imageUrl: imageUrl })
+        })
+        .then(() => {
+          commit('createMeetup', {
+            ...meetup,
+            imageUrl: imageUrl,
+            id: key
+          })
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    updateMeetupData ({ commit }, payload) {
+      commit('setLoading', true)
+      const updateObj = {}
+      if (payload.title) {
+        updateObj.title = payload.title
+      }
+      if (payload.description) {
+        updateObj.description = payload.description
+      }
+      if (payload.date) {
+        updateObj.date = payload.date
+      }
+      firebase.database().ref('meetups').child(payload.id).update(updateObj)
+        .then(() => {
+          commit('setLoading', false)
+          commit('updateMeetup', payload)
+        })
+        .catch(
+        error => {
+          commit('setLoading', false)
+          commit('setError', error)
+          console.log(error)
+        })
+    }
+  },
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedMeetups.sort((meetupA, meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    featuredMeetups (state, getters) {
+      return getters.loadedMeetups.slice(0, 6)
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) => {
+          return meetup.id === meetupId
+        })
+      }
+    }
+  }
+}
